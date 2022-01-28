a7acdca8e2a84603652b6404814b79c9bfc432da
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-01-28 13:16:04 +0100

    essai scale

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderPlaces(places) {
        model.setAttribute('gltf-model', 'assets/magnemite/scene.gltf');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       model.setAttribute('scale', '0.05 0.05 0.05');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
