e03e5a323575b282987c2a2f81d5dc1601d5a6ef
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-06-09 11:05:30 +0200

    Ajout de plusieurs formes pour test

--- js/ar.js
+++ js/ar.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     let places = staticLoadPlaces();
+    console.log(places);
     renderPlaces(places);
 };
 
@@ -12,6 +13,13 @@ function staticLoadPlaces() {
                lng: 6.648636329118053,
            }
        },
+       {
+           name: 'Pikachu',
+           location: {
+               lat: 46.52,
+               lng: 6.7,
+           }
+       }
    ];
 }
 
@@ -22,15 +30,42 @@ function renderPlaces(places) {
        let latitude = place.location.lat;
        let longitude = place.location.lng;
 
-       let model = document.createElement('a-box');
-       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('position', '0 1 0');
-       model.setAttribute('material', 'color: yellow');
+       // on ajoute un cube jaune aux coordonnées indiquées
+       let cube = document.createElement('a-box');
+       cube.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       // il faut imaginer une scène en 3D
+       // la première valeur correspond à x, la deuxième à y (qui est l'altitude), et la troisième à z (qui est la profondeur)
+       cube.setAttribute('position', '0 1 0');
+       cube.setAttribute('material', 'color: yellow');
 
-       model.addEventListener('loaded', () => {
+       cube.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
        });
 
-       scene.appendChild(model);
+       // on ajoute une sphère
+       let sphere = document.createElement('a-sphere');
+       sphere.setAttribute('gps-entity-place', `latitude: ${46.513}; longitude: ${6.648};`);
+       sphere.setAttribute('position', '0 0 0');
+       sphere.setAttribute('material', 'color: green');
+
+       sphere.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+
+       // on ajoute du texte
+       let text = document.createElement('a-text');
+       text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       text.setAttribute('scale', '10 10 10');
+       text.setAttribute('look-at', '[gps-camera]');
+       text.setAttribute('value', 'Coucou toi');
+
+       text.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(cube);
+       scene.appendChild(sphere);
+       scene.appendChild(text);
    });
 }
