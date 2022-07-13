f4fb57e8a73256d64df823cb81acb72d1dac88df
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-07-13 16:55:46 +0200

    ar_files/

--- ar_files/ar_js/ar_step1.js
+++ ar_files/ar_js/ar_step1.js
@@ -1,3 +1,18 @@
 window.addEventListener("arjs-nft-loaded", (event) => {
     // Hide loading overlay
   });
+
+var invocation = new XMLHttpRequest();
+var url = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset';
+var url1 = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset3';
+var url2 = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.iset';
+
+  const express = require('express');
+  const app = express();
+
+
+app.get('https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset', function(request, response) {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.sendFile(__dirname + '/message.json');
+});
+
