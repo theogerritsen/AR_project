561f1df3ca3fb95da67349ea9eee9c83bf6dab66
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-07-25 10:03:49 +0200

    Loader spinner

--- ar_files/ar_js/ar_step1.js
+++ ar_files/ar_js/ar_step1.js
@@ -1,18 +1,19 @@
-window.addEventListener("arjs-nft-loaded", (event) => {
-    // Hide loading overlay
-  });
-
-var invocation = new XMLHttpRequest();
-var url = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset';
-var url1 = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset3';
-var url2 = 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.iset';
-
-  const express = require('express');
-  const app = express();
+// window.addEventListener("arjs-nft-loaded", (event) => {
+//     // Hide loading overlay
+//   });
 
 
-app.get('https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_assets/essai_300dpi.fset', function(request, response) {
-  response.set('Access-Control-Allow-Origin', '*');
-  response.sendFile(__dirname + '/message.json');
-});
 
+// // require(['require', 'express'], function (require){
+// //   const express = require('express');
+// //   const app = express();
+// //   app.use(cors({
+// //     origin: url
+// //   }))
+// // });
+
+// var url = 'https://github.com/theogerritsen/AR_project/tree/main/ar_files/ar_gltf/trex/van/van';
+
+// window.addEventListener("arjs-nft-loaded", (event) => {
+//   // Hide loading overlay
+// });
