c0cd9243d55858822a8ac7129942e7845657c8be
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-08-17 09:29:58 +0200

    test info div

--- ar_files/ar_js/ar_step1.js
+++ ar_files/ar_js/ar_step1.js
@@ -12,18 +12,83 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
-   return [
-       {
-           name: 'Magnemite',
-           location: {
-               lat: 46.51313335530353,
-               lng: 6.648636329118053,
-           }
-       }
-   ];
-}
+    return [
+        {
+            name: 'Magnemite',
+            location: {
+                lat: 46.51313335530353,
+                lng: 6.648636329118053,
+            }
+        }
+    ];
+};
 
-let gest = 'gesture-handler'
+// on va calculer la margin-left après 3 secondes (ar js met une
+// margin-left négative mais prend du temps à charger, même après le load)
+
+let intervalId = window.setTimeout(function() {
+    let body = document.getElementsByTagName('body')[0];
+    let style = window.getComputedStyle(body);
+    let marginLeft =  style.getPropertyValue('margin-left');
+    let margFloat = +(marginLeft.substring(1).slice(0, -2));
+ 
+    document.getElementById("information").setAttribute('style', 'margin-left:'+ margFloat+'px; width:' + window.screen.width + 'px;');
+
+}, 3000);
+
+
+// fonction info div
+
+function toggleActive(clicked_id) {
+
+    // on va chercher notre div qui contiendra les informations
+
+    let infoDiv = document.querySelector('.info');
+
+    // let marginLeft = window.getComputedStyle(document.body).getPropertyValue('margin-left');
+    // let widthInfoDiv = window.getComputedStyle(document.body).getPropertyValue('width');
+    // let len = widthInfoDiv.length;
+    // let pixWidth = +(widthInfoDiv.slice(0, -2));
+    // let margFloat = +(marginLeft.substring(1).slice(0, -2));
+    // console.log('pixwidht', pixWidth);
+    // console.log('margin', margFloat);
+    // let sum = pixWidth - margFloat;
+    // console.log(sum);
+    // console.log(widthInfoDiv.length);
+
+    // document.getElementById("information").setAttribute('style', 'margin-left:'+ marginLeft.substring(1)+';width:'+pixWidth - margFloat+';');
+    //document.getElementById("information").setAttribute('style', 'width:'+ widthInfoDiv.substring(1)+';');
+    let molasse_text = 'je suis de la molasse';
+    let gneiss_text = 'je suis du gneiss';
+
+
+    if (clicked_id == 'molasse') {
+
+        // si le div a deja une classe gneiss ou calcaire, on l'enlève
+        infoDiv.classList.remove("gneiss");
+        infoDiv.classList.remove("calcaire");
+        // on ajoute notre classe molasse
+        infoDiv.classList.toggle("molasse");
+        // on supprime le contenu actuel du div
+        infoDiv.innerHTML = "";
+        // on ajoute le contenu en lien avec la molasse
+        infoDiv.append(molasse_text);
+
+    }
+    if (clicked_id == 'gneiss') {
+
+        infoDiv.classList.remove("molasse");
+        infoDiv.classList.remove("calcaire");
+
+        infoDiv.classList.toggle("gneiss");
+
+        infoDiv.innerHTML = "";
+        
+        infoDiv.append(gneiss_text);
+
+    }
+    //console.log(clicked_id)
+}
 
 function renderPlaces(places) {
     let scene = document.querySelector('#step1');
@@ -37,28 +102,19 @@ function renderPlaces(places) {
         let molasse = document.createElement('a-entity');
         molasse.setAttribute('id', 'molasse');
         molasse.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        molasse.setAttribute('onclick', 'toggleActive(this.id)');
         molasse.setAttribute('gltf-model', '#molasse');
         molasse.setAttribute('scale', '.3 .3 .3');
         molasse.setAttribute('position', '0 1.3 -5');
-        //molasse.setAttribute('gesture-handler', 'locationBased: true');
-        // molasse.setAttribute('event-set__mouseenter', 'class: kaka');
-        // molasse.setAttribute('event-set__mouseleave', 'class: kiki');
         molasse.setAttribute('rotation', '0 0 0');
-        //gneiss.setAttribute('rotate-on-mouseenter');
         molasse.setAttribute("animation__mouseenter", "property: scale; to: .6 .6 .6; dur: 300; startEvents: mouseenter");
         molasse.setAttribute("animation__mouseleave", "property: scale; to: .3 .3 .3; dur: 300; startEvents: mouseleave");
-        //gneiss.setAttribute("animation__position", "property: object3D.position.y; to: 2.2; dir: alternate; dur: 2000; loop: true");
         molasse.setAttribute("animation__rotationenter", "property: object3D.rotation.y; to: 360; dur: 10000; loop: true; easing: linear; startEvents: mouseenter");
         molasse.setAttribute("animation__rotationleave", "property: object3D.rotation.y; to: 0; startEvents: mouseleave");
-
-
-        
 
         molasse.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
-
-
 
         // let molasse_anim = document.createElement("a-entity");
         // molasse_anim.setAttribute('raycaster', 'objects: .clickable');
@@ -71,73 +127,62 @@ function renderPlaces(places) {
         let gneiss = document.createElement('a-entity');
         gneiss.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         gneiss.setAttribute('id', 'gneiss');
-        gneiss.setAttribute('gltf-model', '#gneiss');
+        gneiss.setAttribute('onclick', 'toggleActive(this.id)');
+        gneiss.setAttribute('gltf-model', '#gneiss-texture');
         gneiss.setAttribute('scale', '.3 .3 .3');
         gneiss.setAttribute('position', '1 1.3 -5');
-        //gneiss.setAttribute('gesture-handler', 'locationBased: true');
         gneiss.setAttribute('rotation', '0 0 0');
-        //gneiss.setAttribute('rotate-on-mouseenter');
         gneiss.setAttribute("animation__mouseenter", "property: scale; to: .6 .6 .6; dur: 300; startEvents: mouseenter");
         gneiss.setAttribute("animation__mouseleave", "property: scale; to: .3 .3 .3; dur: 300; startEvents: mouseleave");
-        //gneiss.setAttribute("animation__position", "property: object3D.position.y; to: 2.2; dir: alternate; dur: 2000; loop: true");
         gneiss.setAttribute("animation__rotationenter", "property: object3D.rotation.y; to: 360; dur: 10000; loop: true; easing: linear; startEvents: mouseenter");
         gneiss.setAttribute("animation__rotationleave", "property: object3D.rotation.y; to: 0; startEvents: mouseleave");
-        //gneiss.setAttribute("event-set__mouseenter", 'property: rotation; to 0 360 0; dur 3000; loop: true');
-        //gneiss.setAttribute('event-set__click', 'property: scale; to: .6 .6 .6; dur: 300;')
-        //gneiss.setAttribute("animation__mouseenter", "property: rotation; to: 0 360 0; dur: 3000; loop: true; startEvents: mouseenter");
 
-        gneiss.addEventListener('click', () => {
-            console.log('jai ete clique')
-        });
         gneiss.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
-        // let gneiss = document.createElement('a-entity');
-
-        // gneiss.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        // gneiss.setAttribute('gltf-model', 'ar_gltf/step1/gneiss/scene.gltf');
-        // gneiss.setAttribute('scale', '.3 .3 .3');
-        // gneiss.setAttribute('gesture-handler', 'locationBased: true');
-
-        // gneiss.addEventListener('loaded', () => {
-        //     window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        // });
-
-
         // on ajoute du texte
-        let text = document.createElement('a-text');
-            text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-            text.setAttribute('scale', '10 10 10');
-            text.setAttribute('look-at', '[gps-camera]');
-            text.setAttribute('position', '0 10 0');
-            text.setAttribute('sound', 'on: click; src: #click-sound');
-            text.setAttribute("animation__mouseenter", "property: scale; to: 20 20 20; dur: 300; startEvents: mouseenter");
-            text.setAttribute("animation__mouseleave", "property: scale; to: 10 10 10; dur: 300; startEvents: mouseleave")
+        let molasse_txt = document.createElement('a-text');
+        molasse_txt.setAttribute('id', 'text-molasse');
+        molasse_txt.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        molasse_txt.setAttribute('scale', '1 1 1');
+        molasse_txt.setAttribute('value', 'molasse');
+        molasse_txt.setAttribute('look-at', '[gps-camera]');
+        molasse_txt.setAttribute('position', '-.5 2.1 -5');
+        molasse_txt.setAttribute('sound', 'on: click; src: #click-sound');
 
-        text.addEventListener('loaded', () => {
+        molasse_txt.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
-       //scene.appendChild(text);
-       scene.appendChild(molasse);
-       scene.appendChild(gneiss);
+        let gneiss_txt = document.createElement('a-text');
+        gneiss_txt.setAttribute('id', 'text-gneiss');
+        gneiss_txt.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        gneiss_txt.setAttribute('scale', '1 1 1');
+        gneiss_txt.setAttribute('value', 'gneiss');
+        gneiss_txt.setAttribute('look-at', '[gps-camera]');
+        gneiss_txt.setAttribute('position', '.7 2.1 -5');
+        gneiss_txt.setAttribute('sound', 'on: click; src: #click-sound');
 
-   });
+            gneiss_txt.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(molasse_txt);
+        scene.appendChild(gneiss_txt);
+        scene.appendChild(molasse);
+        scene.appendChild(gneiss);
+
+        gneiss.addEventListener('click', () => {
+
+            console.log('jai ete clique')
+        });
+
+});
 };
 
-AFRAME.registerComponent('rotate-on-mouseenter', {
-    schema: {
-        to: {defaut: '0 360 0', type: 'vec3'}
-    },
 
-    init: function() {
-        let data = this.data;
-        let el = this.el;
-        this.el.addEventListener('mouseenter', function() {
-            el.object3D.rotation.copy(data.to);
-        });
-    }
-});
+
+
 
 
