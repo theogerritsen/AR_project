361c76749a7c37eb0952a33667a8b7322b85bf71
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-09-30 13:25:28 +0200

    2 nft fonctionnent

--- ar_files/ar_js/ar_step3.js
+++ ar_files/ar_js/ar_step3.js
@@ -2,12 +2,11 @@ let scene = document.querySelector('#etape3');
 
 console.log('coucou')
 
-for (var n = 1; n < 10; n++) {
     const nft = document.createElement('a-nft');
-    nft.setAttribute('registerevents', '');
-    nft.setAttribute('name', 'panneau' + n);
+    nft.setAttribute('registerevents', ''   );
+    nft.setAttribute('name', 'panneau2');
     nft.setAttribute('type', 'nft');
-    nft.setAttribute('url', 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_files/ar_gltf/step3/etape3_p' + n);
+    nft.setAttribute('url', 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_files/ar_gltf/step3/etape3_p7');
     nft.setAttribute('smooth', 'true');
     nft.setAttribute('smoothCount', '10');
     nft.setAttribute('smoothTolerance', '.01');
@@ -20,7 +19,8 @@ for (var n = 1; n < 10; n++) {
     infoPlane.setAttribute('height', '1');
     infoPlane.setAttribute('width', '1');
     infoPlane.setAttribute('position', '0 100 -170');
-    infoPlane.setAttribute('scale', '200 200 200');
+    infoPlane.setAttribute('rotation', '-100 0 0');
+    infoPlane.setAttribute('scale', '100 100 100');
     infoPlane.setAttribute('src', 'ar_assets/textures/panneau_molasse.png');
     infoPlane.setAttribute('look-at', '[gps-camera]');
     infoPlane.setAttribute('sound', 'on: click; src: ar_assets/sounds/click.ogg');
@@ -30,7 +30,35 @@ for (var n = 1; n < 10; n++) {
     infoPlane.setAttribute('gesture-handler', 'minScale: 0.25; maxScale: 10');
 
     // on ajoute les NFT à la scène
-    scene.appendChild(nft);
+
     // et on ajoute chaque panneau à l'intérieur du nft
+    scene.appendChild(nft);
     nft.appendChild(infoPlane);
-}
+
+    const nft2 = document.createElement('a-nft');
+    nft2.setAttribute('name', 'panneau');
+    nft2.setAttribute('type', 'nft');
+    nft2.setAttribute('url', 'https://raw.githubusercontent.com/theogerritsen/AR_project/main/ar_files/ar_gltf/step3/etape3_p8');
+    nft2.setAttribute('smooth', 'true');
+    nft2.setAttribute('smoothCount', '10');
+    nft2.setAttribute('smoothTolerance', '.01');
+    nft2.setAttribute('smoothThreshold', '5');
+    //nft.setAttribute('raycaster', 'objects: .clickable');
+    nft2.setAttribute('emitevents', 'true');
+
+    const infoPlane2 = document.createElement('a-plane');
+    infoPlane2.setAttribute('height', '1');
+    infoPlane2.setAttribute('width', '1');
+    infoPlane2.setAttribute('position', '0 100 -170');
+    infoPlane2.setAttribute('scale', '100 100 100');
+    infoPlane2.setAttribute('rotation', '-100 0 0');
+    infoPlane2.setAttribute('src', 'ar_assets/textures/panneau_molasse.png');
+    infoPlane2.setAttribute('look-at', '[gps-camera]');
+    infoPlane2.setAttribute('sound', 'on: click; src: ar_assets/sounds/click.ogg');
+    // infoPlane.setAttribute('event-set__mouseenter', 'scale: 2 2 2');
+    // infoPlane.setAttribute('event-set__mouseleave', '');
+    infoPlane2.setAttribute('class', 'clickable');
+    infoPlane2.setAttribute('gesture-handler', 'minScale: 0.25; maxScale: 10');
+
+    scene.appendChild(nft2);
+    nft2.appendChild(infoPlane2);
